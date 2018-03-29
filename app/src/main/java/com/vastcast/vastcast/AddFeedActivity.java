efa333342e3e6467e37329d2677f59e7f04e5dee
Riley Kohler <rokohler@crimson.ua.edu>
2018-03-29 09:36:11 -0500

    Added Basic AddFeedActivity Functionality

--- app/src/main/java/com/vastcast/vastcast/AddFeedActivity.java
+++ app/src/main/java/com/vastcast/vastcast/AddFeedActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,12 +20,14 @@ public class AddFeedActivity extends AppCompatActivity {
 
         /*TODO: Add a listener to launch RetrieveFeedTask on button press*/
         /*TODO: Check Settings to determine connection status and preference*/
-        try {
-            new RetrieveFeedTask().execute("https://rss.simplecast.com/podcasts/1497/rss").get();
-        }
-        catch(Exception e) {
-            Log.e("RSS", Log.getStackTraceString(e));
-        }
+        Button btnEnterUrl = findViewById(R.id.btnEnterUrl);
+        final EditText txtUrl = findViewById(R.id.txtUrl);
+        btnEnterUrl.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                new RetrieveFeedTask().execute(txtUrl.getText().toString());
+                //new RetrieveFeedTask().execute("https://rss.simplecast.com/podcasts/1497/rss");
+            }
+        });
     }
 }
 
@@ -45,9 +50,11 @@ class RetrieveFeedTask extends AsyncTask<String, Void, Collection> {
             //Handle errors in reading a feed
             Log.e("RSS", Log.getStackTraceString(e));
         }
-        /*TODO: Launch new activity with the resulting podcast details*/
-        Log.d("Eps", Integer.toString(c.getEpisodes().size()));
-        printEpisodes(c);
+        else {
+            /*TODO: Launch new activity with the resulting podcast details*/
+            Log.d("Eps", Integer.toString(c.getEpisodes().size()));
+            printEpisodes(c);
+        }
     }
 
     private void printEpisodes(Collection podcast) {
