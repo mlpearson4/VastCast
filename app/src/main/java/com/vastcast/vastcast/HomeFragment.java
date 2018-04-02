32e38a5e78ce180cc821443e7171d3aea7b73b14
Riley Kohler <rokohler@crimson.ua.edu>
2018-04-02 12:46:38 -0500

    Style Changes to AddPodcast to Play Episode flow

--- app/src/main/java/com/vastcast/vastcast/HomeFragment.java
+++ app/src/main/java/com/vastcast/vastcast/HomeFragment.java
@@ -1,12 +1,14 @@
 package com.vastcast.vastcast;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -64,7 +66,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+                Button btnRssFetch = (Button) view.findViewById(R.id.btnRSSFetch);
+        btnRssFetch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                HomeFragment.this.startActivity(new Intent(getActivity(), AddFeedActivity.class));
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
