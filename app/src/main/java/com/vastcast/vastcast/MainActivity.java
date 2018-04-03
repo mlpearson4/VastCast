9aab2d05637dfbca65e1ecbb69066647ce7277e9
Raychel <rhdelaney@crimson.ua.edu>
2018-04-03 08:35:59 -0500

    Database added details will be shared in gm right now about email to access and then I can add all the features before the meeting easy

--- app/src/main/java/com/vastcast/vastcast/MainActivity.java
+++ app/src/main/java/com/vastcast/vastcast/MainActivity.java
@@ -7,7 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
