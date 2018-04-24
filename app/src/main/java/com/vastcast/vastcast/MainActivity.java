178ce0216883035492bdc126b98f9b97c9c0536a
Michelle Pearson <mlpearson4@crimson.ua.edu>
2018-04-24 15:04:40 -0500

    started adding search button

--- app/src/main/java/com/vastcast/vastcast/MainActivity.java
+++ app/src/main/java/com/vastcast/vastcast/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
     /*TODO: Add Search to Menu*/
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
         inflater.inflate(R.menu.menu_settings, menu);
         return true;
     }
