da8c6068fa30d30e3756e46419e748e36c15daf1
ekpope1 <ekpope1@crimson.ua.edu>
2018-04-25 14:23:54 -0500

    SettingsActivity simplified to remove unnecessary fragment

--- app/src/main/java/com/vastcast/vastcast/SettingsActivity.java
+++ app/src/main/java/com/vastcast/vastcast/SettingsActivity.java
@@ -2,28 +2,13 @@ package com.vastcast.vastcast;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 import android.util.Log;
 
+
 public class SettingsActivity extends PreferenceActivity {
-
-    public SettingsActivity() {}
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("SettingsActivity", "onCreate");
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment()).commit();
-    }
-
-    public static class SettingsFragment extends PreferenceFragment {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            Log.d("SettingsFragment", "onCreate");
-            addPreferencesFromResource(R.xml.preferences);
-        }
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
