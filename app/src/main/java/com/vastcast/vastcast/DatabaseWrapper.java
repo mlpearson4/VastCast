4a68fb80f10fff3dd11cf16a664b3d25aae71711
Raychel <rhdelaney@crimson.ua.edu>
2018-04-24 21:45:26 -0500

    this is a shitty dbwrapper

--- app/src/main/java/com/vastcast/vastcast/DatabaseWrapper.java
+++ app/src/main/java/com/vastcast/vastcast/DatabaseWrapper.java
@@ -34,73 +34,19 @@ public class DatabaseWrapper {
         myUser=myRef.child("Users");
         initStat=true;
     }
-    /*public static Collection checkCollection(URL source){
-        String s=source.toString();
-        String result = null;
-        try
-        {
-            result = URLEncoder.encode(s, "UTF-8")
-                    .replaceAll("\\+", "%20")
-                    .replaceAll("\\%21", "!")
-                    .replaceAll("\\%27", "'")
-                    .replaceAll("\\%28", "(")
-                    .replaceAll("\\%29", ")")
-                    .replaceAll("\\%7E", "~")
-                    .replaceAll("\\.","%2E");
-        }
 
-        // This exception should never occur.
-        catch (UnsupportedEncodingException e)
-        {
-            result = s;
-        }
-        /*Query q=myData.equalTo(result).addListenerForSingleValueEvent();
-        );
-        if(q!=null){
-            //return results from query right here
-            //Collection c = q.getClass(Collection);
-            return c;
-        }
-        return null;
-    }*/
     public static void addCollection(Collection col){
         if(!initStat) {
             initializeDB();
         }
-       /* Log.d("DatabaseWrapper","pre-encoded");
-        String result = null;
-
-        try
-        {
-            result = URLEncoder.encode(col.getSource(), "UTF-8")
-                    .replaceAll("\\+", "%20")
-                    .replaceAll("\\%21", "!")
-                    .replaceAll("\\%27", "'")
-                    .replaceAll("\\%28", "(")
-                    .replaceAll("\\%29", ")")
-                    .replaceAll("\\%7E", "~")
-                    .replaceAll("\\.","%2E");
-        }
-
-        // This exception should never occur.
-        catch (UnsupportedEncodingException e)
-        {
-            result = col.getSource();
-        }*/
-       //String result = URLEncoder.encode(col.getSource(), "UTF-8");
-
-        //pushing Collection
-        //myData.child(result).setValue(col);
         myData.push().setValue(col);
-        //Put collection ID into Users Library
-        myUser.child(userID).child("Playlists").push().setValue(col);
-        //myUser.child(userID).child("Playlists").setValue(result);
+        //myUser.child(userID).child("Playlists").push().setValue(col);
     }
     public static void addToUserLibrary(Collection col){
         if(!initStat) {
             initializeDB();
         }
-        myUser.child(userID).child("Playlists").push().setValue(col);
+       myUser.child(userID).child("Library").push().setValue(col);
     }
     public static void addToQueue(Collection col){
         if(!initStat) {
@@ -109,20 +55,15 @@ public class DatabaseWrapper {
         myUser.child(userID).child("Queue").push().setValue(col);
     }
     public static void updateCollection(Collection col){
-        //this isn't entirely right atm
-        //find key
 
-        //then set value at key
-        //myData.child("<keyishere>").setValue(col);
     }
     public static void updateQueue(Collection col){
-        //this isn't entirely right atm
-        //myRef.child("Users").child(userID).child("Queue").setValue(col);
+
     }
     public static void removeFromCollection(Collection col){
-        // myRef.child("Users").child(userID).child("Library").removeValue();
+
     }
     public static void removeFromQueue(Collection col){
-        // myRef.child("Users").child(userID).child("Queue").removeValue();
+
     }
 }
